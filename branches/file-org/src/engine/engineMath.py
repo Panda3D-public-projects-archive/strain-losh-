88bdcc28f5e322cb9a3e6f7a663e5961d3269377
ogi.losh@gmail.com <ogi.losh@gmail.com@4f0e03c3-8530-09c2-737e-33c5a6d01d0a>
2011-11-02 10:15:58 +0000

    -testing different file organization

---
+++ branches/file-org/src/engine/engineMath.py
@@ -0,0 +1,124 @@
+import random
+import math
+from engine.unit import Unit
+
+
+
+_PI = math.pi
+_PI_2 = _PI / 2
+_3_PI_2 = 3 * _PI / 2
+_PI_4 = _PI / 4
+_3_PI_4 = 3 * _PI / 4 
+_5_PI_4 = 5 * _PI / 4
+_2_PI = 2 * _PI
+_PI_8 = _PI / 8
+_3_PI_8 = 3 * _PI / 8
+_5_PI_8 = 5 * _PI / 8
+_7_PI_8 = 7 * _PI / 8
+
+
+
+def getHeading( myPosition, lookAtPoint ):
+    
+    #trivial check if this is the same position, if it is, return none
+    if myPosition == lookAtPoint:
+        return Unit.HEADING_NONE
+    
+    angle = math.atan2( lookAtPoint[1] - myPosition[1] , lookAtPoint[0] - myPosition[0] )
+
+    if angle < -_7_PI_8:
+        return Unit.HEADING_W
+    elif angle < -_5_PI_8:
+        return Unit.HEADING_SW
+    elif angle < -_3_PI_8:
+        return Unit.HEADING_S
+    elif angle < -_PI_8:
+        return Unit.HEADING_SE
+    elif angle < _PI_8:
+        return Unit.HEADING_E
+    elif angle < _3_PI_8:
+        return Unit.HEADING_NE
+    elif angle < _5_PI_8:
+        return Unit.HEADING_N
+    elif angle < _7_PI_8:
+        return Unit.HEADING_NW
+    
+    return Unit.HEADING_W
+    
+
+
+
+class Weapon():
+    
+    def __init__(self):
+    
+        self.range = 24
+        self.type = TYPE_RIFLE
+    
+
+
+TYPE_RIFLE = 1
+RANGE = 24
+BS = 4
+
+def signum( num ):
+    if( num < 0 ): 
+        return -1
+    elif( num >= 0 ):
+        return 1
+
+
+
+
+def givePercent( weapon, distance, bs ):
+    
+    point_blank = (bs-1)
+    if distance <= point_blank:
+        return 100
+    
+    t = math.pi / (weapon.range-point_blank) 
+    x = (distance-point_blank) * t - math.pi / 2
+
+    sin = math.sin(-x)
+    factor = bs * 7
+
+    if sin > 0:
+        res = math.sin(-x) * (70 - factor) 
+    else:
+        res = math.sin(-x) * (25 + factor ) 
+    
+    res = res + 30 + factor
+    
+    return res
+
+
+def d( num, dice ):
+    ret = 0
+    for a in xrange( num ):
+        ret += random.randint( 1, dice )
+    return ret
+
+
+def d100():
+    return d(1,100)
+
+random.random()
+
+
+def percentList( bs, weapon = None ):
+    a = []
+    if not weapon: 
+        wpn = Weapon()
+    else:
+        wpn = weapon
+        
+    for i in xrange( 1,25 ):
+        a.append( (i, givePercent( wpn, i, bs ) ) )
+        #print a[-1]
+    
+    return a
+
+def distance( x1, y1, x2, y2 ):    
+    return math.sqrt( math.pow( (x2-x1) , 2) +  math.pow( (y2-y1) , 2) )
+    
+    
---
+++ branches/file-org/src/messaging.py
@@ -0,0 +1,454 @@
+'''
+Created on 4.10.2011.
+
+@author: krav
+'''
+import sys
+import cPickle as pickle
+from pandac.PandaModules import QueuedConnectionManager, QueuedConnectionListener, QueuedConnectionReader, ConnectionWriter #@UnresolvedImport
+from pandac.PandaModules import NetAddress, NetDatagram, PointerToConnection #@UnresolvedImport
+from direct.distributed.PyDatagramIterator import PyDatagramIterator
+import engine.engineCore 
+import time
+import threading
+import collections
+
+
+
+
+IP_ADDRESS = 'localhost'
+#IP_ADDRESS = 'krav.servebeer.com'
+#NAME = 'blood angels'
+NAME = 'ultramarines'
+TCP_PORT = 56005
+
+
+class Msg:
+
+
+    MOVE = 1                #values - list of move actions ('move',tile) followed by ('rotate',tile)
+    NEW_TURN = 2            #value - turn number
+    ENGINE_SHUTDOWN = 3     #no values
+    ERROR = 4               #value - error message
+    ENGINE_STATE = 5        #value - dictionary of values
+    LEVEL = 6               #value - pickled level
+    END_TURN = 7            #no values
+    UNIT = 8                #value - pickled unit
+
+    
+    
+    def __init__(self, in_type, value=None):
+        self.type = in_type
+        self.values = value
+
+    def __repr__(self):
+        if self.type == Msg.ENGINE_STATE:
+            return "type:ENGINE_STATE"
+        elif self.type == Msg.MOVE:
+            return "type:MOVE, value:%s" % str(self.values)
+        elif self.type == Msg.NEW_TURN:
+            return "type:NEW_TURN, value:%s" % str(self.values)
+        elif self.type == Msg.ERROR:
+            return "type:ERROR, value:%s" % str(self.values)
+        elif self.type == Msg.LEVEL:
+            return "type:LEVEL"
+        elif self.type == Msg.END_TURN:
+            return "type:END_TURN, value:%s" % str(self.values)
+        elif self.type == Msg.UNIT:
+            return "type:UNIT"  
+        
+        return "type:%d  value:%s" % (self.type, str(self.values))
+
+
+
+class EngMsg:
+    
+    cManager = None
+    cListener = None
+    cReader = None
+    cWriter = None
+    tcpSocket = None
+     
+    activeConnections = []
+    
+    handshakedConnections = collections.deque()
+    
+    log = None
+    
+    @staticmethod
+    def startServer( lgr ):
+        EngMsg.cManager = QueuedConnectionManager()
+        EngMsg.cListener = QueuedConnectionListener(EngMsg.cManager, 0)
+        EngMsg.cReader = QueuedConnectionReader(EngMsg.cManager, 0)
+        EngMsg.cWriter = ConnectionWriter(EngMsg.cManager, 0)
+
+        EngMsg.log = lgr
+  
+        backlog = 5
+        EngMsg.tcpSocket = EngMsg.cManager.openTCPServerRendezvous(TCP_PORT , backlog)        
+        
+        EngMsg.cListener.addConnection(EngMsg.tcpSocket)
+    
+    @staticmethod
+    def stopServer():
+        for client, address, name in EngMsg.activeConnections[:]:
+            EngMsg.disconnect(client, address, name)
+            EngMsg.logger.notify.info("Closing connection with: %s @ %s", name, address)
+    
+        EngMsg.activeConnections = []
+        
+        EngMsg.cManager = None
+        EngMsg.cListener = None
+        EngMsg.cReader = None
+        EngMsg.cWriter = None
+        EngMsg.tcpSocket = None
+     
+        EngMsg.handshakedConnections = None
+    
+    @staticmethod
+    def handleConnections():
+        
+        #check for new connections
+        if EngMsg.cListener.newConnectionAvailable():
+        
+            rendezvous = PointerToConnection()
+            netAddress = NetAddress()
+            newConnection = PointerToConnection()
+        
+            if EngMsg.cListener.getNewConnection(rendezvous, netAddress, newConnection):
+                newConnection = newConnection.p()
+                newConnection.setNoDelay(1)
+                              
+                #try handshaking
+                threading.Thread(target=EngMsg.handshake, args=( [newConnection, engine.engineCore.Engine.getInstance().players[:]] ) ).start()
+
+                
+                 
+        #check if we have a new handshaked connection
+        try:
+            conn, success, player_name = EngMsg.handshakedConnections.pop()            
+            if success:
+                
+                #go through all players
+                for player in engine.engineCore.Engine.getInstance().players:
+                    if player.name == player_name:
+                        
+                        #see if there is already a connection for this player, if yes than disconnect it
+                        if player.connection != None:
+                            EngMsg.sendErrorMsg("Disconnecting because this player is connecting from other connection", player.connection )
+                            EngMsg.disconnect( player.connection, player.connection.getAddress(), player.name )
+                            EngMsg.log.info("Player %s disconnected because he was logging in from other connection", player.name)
+                            
+                        #remember this connection
+                        player.connection = conn
+                
+                EngMsg.cReader.addConnection(conn)
+                EngMsg.log.info("Client connected: %s @ %s", player_name, conn.getAddress() )
+                EngMsg.activeConnections.append( ( conn, conn.getAddress(), player_name ) )
+                
+            else:
+                #in this case, address and name parameter don't matter, cause there is nothing in activeConnections yet
+                EngMsg.disconnect( conn, conn.getAddress(),'' )
+                EngMsg.log.info("Client didn't pass handshaking.")                
+        except IndexError:
+            pass
+                   
+                   
+        #check for disconnects
+        for connection, address, name in EngMsg.activeConnections[:]:   
+            if not connection.getSocket().Active():
+                EngMsg.log.info("Client disconnected: %s @ %s", name, address)
+                EngMsg.disconnect(connection, address, name)
+    
+    
+    @staticmethod
+    def disconnect( connection, address, name ):
+            EngMsg.cListener.removeConnection( connection )
+            EngMsg.cReader.removeConnection( connection )
+            EngMsg.cManager.closeConnection( connection )
+            try:
+                EngMsg.activeConnections.remove( ( connection, address, name ) )
+            except:
+                pass
+        
+    
+    @staticmethod
+    def getData( socket, timeout ):
+        t = time.time()        
+        while 1:
+            msg = socket.RecvData(1024)
+            if msg:
+                return msg            
+            if( time.time() - t > timeout ):
+                return None
+            time.sleep(0.01)
+        return None
+    
+    
+    @staticmethod
+    def handshake( connection, players ):
+        s = connection.getSocket()
+
+        if EngMsg.getData(s, 3) != 'LOSH?':
+            EngMsg.handshakedConnections.append( (connection, False, '') )
+            return
+            
+        s.SendData('LOSH!')
+
+        if EngMsg.getData(s, 3) != 'Strain?':
+            EngMsg.handshakedConnections.append( (connection, False, '') )
+            return
+             
+        s.SendData('Send your name')        
+        
+        #check to see if there is a player with that name
+        name = EngMsg.getData(s, 3)
+        found = False        
+        for player in players:
+            if name == player.name:
+                found = True
+                break
+                
+        if not found:
+            EngMsg.handshakedConnections.append( (connection, False, '') )
+            return        
+        
+        s.SendData('Welcome')
+        
+        EngMsg.handshakedConnections.append( (connection, True, name) )
+        return
+                
+    @staticmethod
+    def broadcastMsg(msg):
+
+        # broadcast a message to all clients
+        for client, address, name in EngMsg.activeConnections: 
+            netDatagram = NetDatagram()
+            netDatagram.addString(pickle.dumps(msg))
+            if EngMsg.cWriter.send(netDatagram, client):
+                EngMsg.log.debug( "Sent client: %s @ %s\tmessage:%s" , name, address, msg )
+        
+    
+    @staticmethod
+    def readMsg():
+        """Return the message, if any, or None if there was nothing to read"""
+        if EngMsg.cReader.dataAvailable():
+            datagram = NetDatagram() 
+            if EngMsg.cReader.getData(datagram):
+                dgi = PyDatagramIterator(datagram)
+                msg = pickle.loads(dgi.getString())
+                EngMsg.log.info("Engine received a message:%s, from:%s", msg, str(datagram.getConnection().getAddress()))
+                return (msg, datagram.getConnection() )
+
+          
+        return None
+          
+    @staticmethod
+    def _sendMsg(msg, source = None):
+        try:
+            if source:
+                for conn, address, name in EngMsg.activeConnections:
+                    if conn == source:
+                        netDatagram = NetDatagram()
+                        netDatagram.addString(pickle.dumps(msg))
+                        if EngMsg.cWriter.send(netDatagram, conn):
+                            EngMsg.log.debug( "Sent client: %s @ %s\tmessage:%s" , name, address, msg )
+            else:
+                EngMsg.broadcastMsg(msg)
+        except:
+            EngMsg.log.critical("Could not send message to clients, reason : %s", sys.exc_info()[1])
+            return
+        
+        EngMsg.log.info("Engine posted a message: %s" , msg )
+    
+    @staticmethod
+    def move(unit_id, move_actions):
+        EngMsg._sendMsg(Msg(Msg.MOVE, (unit_id, move_actions))) 
+                                                                      
+    @staticmethod
+    def sendState(engine_state, source):
+        EngMsg._sendMsg(Msg(Msg.ENGINE_STATE, engine_state), source)
+    
+    @staticmethod
+    def sendLevel(pickled_level):
+        EngMsg._sendMsg(Msg(Msg.LEVEL, pickled_level))
+    
+    @staticmethod
+    def sendErrorMsg(error_msg, source = None):
+        EngMsg._sendMsg(Msg(Msg.ERROR, error_msg), source)
+    
+    @staticmethod
+    def sendNewTurn(turn_num):
+        EngMsg._sendMsg(Msg(Msg.NEW_TURN, turn_num))
+    
+    @staticmethod
+    def sendUnit(pickled_unit):
+        EngMsg._sendMsg(Msg(Msg.UNIT, pickled_unit))
+    
+    
+    
+    
+class ClientMsg:
+    
+    
+    cManager = None
+    cListener = None
+    cReader = None
+    cWriter = None
+    tcpSocket = None
+    myConnection = None
+    
+    num_failed_attempts = 0
+
+    log = None
+
+    @staticmethod
+    def connect():
+        ClientMsg.log.info( "Trying to connect to server: %s:%s", IP_ADDRESS, TCP_PORT )
+        
+        ClientMsg.cManager = QueuedConnectionManager()
+        ClientMsg.cReader = QueuedConnectionReader(ClientMsg.cManager, 0)
+        ClientMsg.cWriter = ConnectionWriter(ClientMsg.cManager, 0)
+        
+        # how long until we give up trying to reach the server?
+        timeout_in_miliseconds = 3000  # 3 seconds
+        ClientMsg.myConnection = ClientMsg.cManager.openTCPClientConnection(IP_ADDRESS, TCP_PORT, timeout_in_miliseconds)
+        
+        if ClientMsg.myConnection:
+            ClientMsg.myConnection.setNoDelay(1)
+            
+            #try handshaking
+            if not ClientMsg.handshake():
+                ClientMsg.disconnect()
+                ClientMsg.log.error( "Did not pass handshake.")
+                return False
+            
+            ClientMsg.cReader.addConnection(ClientMsg.myConnection)
+            
+            ClientMsg.log.info( "Connected to server: %s", ClientMsg.myConnection.getAddress() )
+            return True
+            
+        return False
+            
+            
+    @staticmethod
+    def disconnect():
+        if ClientMsg.myConnection:
+            ClientMsg.cReader.removeConnection( ClientMsg.myConnection )
+            ClientMsg.cManager.closeConnection( ClientMsg.myConnection )
+            
+        ClientMsg.myConnection = None
+            
+            
+    @staticmethod
+    def handleConnection():
+        """Return True if connection is ok, returns False if there is no connection."""
+        
+        #if we are not connected, try to connect
+        if not ClientMsg.myConnection:
+            
+            if ClientMsg.num_failed_attempts > 5:
+                return False
+            
+            #if we already tried 5 times, don't even bother
+            if ClientMsg.num_failed_attempts == 5:
+                ClientMsg.log.error("Failed to connect %d times, giving up.", ClientMsg.num_failed_attempts)
+                ClientMsg.num_failed_attempts += 1
+                return False
+            
+            if ClientMsg.connect():
+                #every time we reconnect to server, get the engine state 
+                ClientMsg.getEngineState()
+                ClientMsg.num_failed_attempts = 0             
+                return True
+            else:
+                ClientMsg.num_failed_attempts += 1                
+                return False
+            
+        
+        #check the connection, if there is none, disconnect everything and return false
+        if not ClientMsg.myConnection.getSocket().Active():
+            ClientMsg.log.error( "Lost connection to server: %s", IP_ADDRESS )
+            ClientMsg.disconnect()
+            return False
+
+        #we are connected and everything is ok
+        return True
+            
+    @staticmethod
+    def handshake():
+        s = ClientMsg.myConnection.getSocket()
+
+        s.SendData('LOSH?')        
+        
+        if EngMsg.getData(s, 2) != 'LOSH!':
+            return False
+
+        s.SendData('Strain?')
+                
+        if EngMsg.getData(s, 2) != 'Send your name':
+            return False
+
+        s.SendData(NAME)
+
+        welcomeMsg = EngMsg.getData(s, 2)
+        
+        if welcomeMsg != 'Welcome':
+            return False
+        
+        return True
+            
+    @staticmethod
+    def readMsg():
+        """Return the message, if any, or None if there was nothing to read"""
+        
+        if not ClientMsg.myConnection:
+            return None
+        
+        if ClientMsg.cReader.dataAvailable():
+            datagram = NetDatagram()                          
+            if ClientMsg.cReader.getData(datagram):
+                dgi = PyDatagramIterator(datagram)                
+                msg = pickle.loads(dgi.getString())                
+                ClientMsg.log.info("Client received a message:%s", msg)
+                          
+                return msg
+                                            
+        return None    
+
+        
+    @staticmethod
+    def _sendMsg(msg):        
+
+        if not ClientMsg.myConnection:
+            return
+         
+        datagram = NetDatagram()        
+        datagram.addString(pickle.dumps(msg, pickle.HIGHEST_PROTOCOL))   
+        ClientMsg.cWriter.send(datagram, ClientMsg.myConnection)
+        ClientMsg.log.debug("Client posted a message: %s", msg)
+
+    
+    @staticmethod
+    def getEngineState():
+        ClientMsg._sendMsg(Msg(Msg.ENGINE_STATE))
+    
+    @staticmethod
+    def getLevel():
+        ClientMsg._sendMsg(Msg(Msg.LEVEL))
+    
+    @staticmethod
+    def move(unit_id, new_position, orientation):
+        ClientMsg._sendMsg(Msg(Msg.MOVE, { 'unit_id':unit_id,
+                                           'new_position':new_position,
+                                           'orientation':orientation })) 
+
+    @staticmethod
+    def shutdownEngine():
+        ClientMsg._sendMsg(Msg(Msg.ENGINE_SHUTDOWN)) 
+        
+    @staticmethod
+    def endTurn():
+        ClientMsg._sendMsg(Msg(Msg.END_TURN))
+        
+        
