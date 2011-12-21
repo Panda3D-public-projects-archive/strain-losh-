00865d96d779727160247b33d2aec2349544e1de
ogi.losh@gmail.com <ogi.losh@gmail.com>
2011-12-21 20:42:55 +0000

    -stopped camera from moving when console has focus

--- Strain/client/main.py
+++ Strain/client/main.py
@@ -541,7 +541,7 @@ class Client(DirectObject):
         
         # Init Camera
         #self.camera = Camera(self)
-        self.camera = Camera(20, 20)
+        self.camera = Camera(self, 20, 20)
         
         # Init Interface
         self.interface = Interface(self)
@@ -697,91 +697,11 @@ class Client(DirectObject):
             if u['pos'][0] == pos.getX() and u['pos'][1] == pos.getY():
                 return u['id']
         return None
-
-    def outOfLevelBounds(self, x, y):
-        if(x < 0 or y < 0 or x >= self.level.maxX or y >= self.level.maxY):
-            return True
-        else:
-            return False
-
-    def canIMoveHere(self, unit, position, dx, dy):
-        dx = int(dx)
-        dy = int(dy)
-              
-        if( (dx != 1 and dx != 0 and dx != -1) and 
-            (dy != 1 and dy != 0 and dy != -1) ):
-            print ( "Invalid dx (%d) or dy (%d)" %(dy ,dy) )
-        
-        ptx = int(position[0])
-        pty = int(position[1])
-        
-        if self.outOfLevelBounds(ptx+dx, pty+dy):
-            return False
-
-        #check if the level is clear at that tile
-        if(self.level.getHeight( (ptx + dx), (pty + dy) ) != 0):
-            return False
-        
-        #check if there is a dynamic obstacle in the way
-        for unit in self.units.itervalues():  
-            if unit['pos'][0] == ptx+dx and unit['pos'][1] == pty+dy:
-                return False
-        
-        #check diagonal if it is clear
-        if( dx != 0 and dy != 0 ):
-            
-            #if there is something in level in the way
-            if( self.level.getHeight( (ptx + dx), (pty) ) != 0 or 
-                self.level.getHeight( (ptx), (pty + dy) ) != 0 ):
-                return False
-            
-        return True
-        
-    def getMoveDict(self, unit_id, returnOriginTile=False):
-        unit = self.units[unit_id]
-        final_dict = {}
-        open_list = [(unit['pos'], unit['ap'])]
-        for tile, actionpoints in open_list:
-            for dx in xrange(-1,2):
-                for dy in xrange( -1,2 ):            
-                    if( dx == 0 and dy == 0):
-                        continue
-                    #we can't check our starting position
-                    if( tile[0] + dx == unit['pos'][0] and tile[1] + dy == unit['pos'][1] ):
-                        continue
-                    x = int( tile[0] + dx )
-                    y = int( tile[1] + dy )
-                    if self.outOfLevelBounds(x, y):
-                        continue
-                    if not self.canIMoveHere(unit, tile, dx, dy):
-                        continue                   
-                    #if we are checking diagonally
-                    if( dx == dy or dx == -dy ):
-                        ap = actionpoints - 1.5
-                    else:
-                        ap = actionpoints - 1
-                    
-                    if( ap < 0 ):
-                        continue
-                    
-                    pt = (x,y) 
-                    
-                    if pt in final_dict:
-                        if( final_dict[pt] < ap ):
-                            final_dict[pt] = ap
-                            open_list.append( ( pt, ap ) )
-                    else: 
-                            final_dict[pt] = ap
-                            open_list.append( ( pt, ap ) )
-        if( returnOriginTile ):
-            final_dict[unit.pos] = unit.ap
-            return final_dict
-        
-        return final_dict
     
     def beforeAnimHook(self):
         self._anim_in_process = True
         self.sgm.hideUnitAvailMove()
+        self.sgm.hideVisibleEnemies()
     
     def afterAnimHook(self):
         self._anim_in_process = False
