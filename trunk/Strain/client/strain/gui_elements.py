2db24ed9e2b1417be1a24ec91654928abedd82b0
vznidar@gmail.com <vznidar@gmail.com@4f0e03c3-8530-09c2-737e-33c5a6d01d0a>
2011-12-26 13:47:13 +0000

    -new file which holds gui elements' classes, should be included in interface.py

---
+++ trunk/Strain/client/strain/gui_elements.py
@@ -0,0 +1,262 @@
+'''
+Created on 26. pro. 2011.
+
+@author: Vjeko
+'''
+from panda3d.core import NodePath, CardMaker, GeomNode, TransparencyAttrib, TextNode#@UnresolvedImport
+from direct.gui.DirectGui import DirectFrame, DGG
+from direct.gui.OnscreenText import OnscreenText
+from direct.gui.OnscreenImage import OnscreenImage
+
+#===============================================================================
+# CLASS GuiCard --- DEFINITION
+#===============================================================================
+class GuiCard:
+    def __init__(self, width, height, border_size, pos, hugpos, color):
+        self.hugpos = hugpos
+        self.width = width
+        self.height = height
+        self.border_size = border_size
+        self.pos = pos
+        self.node = NodePath("guicard")
+        self.frame_node = NodePath("frame")
+        cm = CardMaker("cm_left")
+        cm.setFrame(0, border_size, 0, height)
+        n = self.frame_node.attachNewNode(cm.generate())
+        n.setPos(0, 0, 0)
+        cm = CardMaker("cm_right")
+        cm.setFrame(0, border_size, 0, height)
+        n = self.frame_node.attachNewNode(cm.generate())
+        n.setPos(width - border_size, 0, 0)
+        cm = CardMaker("cm_top")
+        cm.setFrame(0, width, height - border_size, width)
+        n = self.frame_node.attachNewNode(cm.generate())
+        n.setPos(0, 0, 0)
+        cm = CardMaker("cm_bottom")
+        cm.setFrame(0, width, 0, border_size)
+        n = self.frame_node.attachNewNode(cm.generate())
+        n.setPos(0, 0, 0)
+        self.frame_node.setColor(color)
+        self.frame_node.flattenStrong()
+        self.frame_node.reparentTo(self.node)
+        cm = CardMaker("cm_back")
+        cm.setFrame(0+border_size, width-border_size, 0+border_size, height-border_size)
+        self.back_node = self.node.attachNewNode(cm.generate())
+        self.back_node.setPos(0, 0, 0)
+        self.back_node.setTransparency(1)
+        self.node.reparentTo(aspect2d)#@UndefinedVariable
+        self.redraw()
+    
+    def setTexture(self, tex):
+        self.back_node.setTexture(tex)
+    
+    def redraw(self):
+        if self.hugpos == "topleft":
+            p = base.a2dTopLeft.getPos()#@UndefinedVariable
+            p.setZ(p.getZ() - self.height)
+        elif self.hugpos == "topright":
+            p = base.a2dTopRight.getPos()#@UndefinedVariable
+            p.setZ(p.getZ() - self.height)
+        elif self.hugpos == None:
+            p = self.pos
+        self.node.setPos(p)
+    
+    def removeNode(self):
+        self.node.removeNode()
+
+#===============================================================================
+# CLASS GuiButton --- DEFINITION
+#===============================================================================
+class GuiButton:
+    def __init__(self, hugpos, offset, aspect, plane, name):
+        self.enabled = True
+        self.name = name
+        self.node = aspect2d.attachNewNode("guibutton")#@UndefinedVariable
+        self.node.setTransparency(TransparencyAttrib.MAlpha)
+        self.node.setAlphaScale(1) 
+        geom = GeomNode('plane')
+        geom.addGeomsFrom(plane.getChild(0).getChild(0).node())
+        self.frame = self.node.attachNewNode(geom) 
+        self.frame.setScale(0.05)
+        self.node.setTexture(loader.loadTexture(name+".png"))#@UndefinedVariable
+        self.hugpos = hugpos
+        self.offset = offset
+        self.redraw(aspect)
+
+    def redraw(self, aspect, flag="wide"):
+        if self.hugpos == "topleft":
+            p = base.a2dTopLeft.getPos()#@UndefinedVariable
+            p.setX(p.getX() + self.offset.getX() + 0.05)
+            p.setZ(p.getZ() + self.offset.getZ() - 0.05)
+        self.frame.setPos(p)
+        if flag == "wide":
+            posx, posy = self.frame.getTightBounds()
+            self.pos_min_x = posx.getX() / aspect
+            self.pos_min_y = posx.getZ()
+            self.pos_max_x = posy.getX() / aspect
+            self.pos_max_y = posy.getZ()
+        elif flag == "tall":
+            posx, posy = self.frame.getTightBounds()
+            self.pos_min_x = posx.getX()
+            self.pos_min_y = posx.getZ() / aspect
+            self.pos_max_x = posy.getX()
+            self.pos_max_y = posy.getZ() / aspect
+            
+    def turnOn(self):
+        self.node.setTexture(loader.loadTexture(self.name+"_on.png"))#@UndefinedVariable
+        
+    def turnOff(self):
+        self.node.setTexture(loader.loadTexture(self.name+".png"))#@UndefinedVariable    
+            
+    def enable(self):
+        self.node.setTexture(loader.loadTexture(self.name+".png"))#@UndefinedVariable
+        self.enabled = True
+        
+    def disable(self):
+        self.node.setTexture(loader.loadTexture("empty.png"))#@UndefinedVariable
+        self.enabled = False
+            
+    def removeNode(self):
+            self.node.removeNode()   
+
+#===============================================================================
+# CLASS GuiButton2 --- DEFINITION
+#===============================================================================
+class GuiButton2:
+    def __init__(self, hugpos, offset, aspect, plane, name):
+        self.enabled = True
+        self.name = name
+        
+        self.frame = DirectFrame(   relief = DGG.FLAT
+                                  #, frameColor = (0.2, 0.2, 0.2, 0.8)
+                                  , scale = 1
+                                  , frameSize = (-0.0533, 0.0533, -0.0533, 0.0533) ) #32/600=0.05333
+        self.frame.setScale(0.0533)
+        
+        self.imageObject = OnscreenImage(image = name+".png", pos = (0, 0, 0))
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        self.imageObject.setAlphaScale(1) 
+        self.imageObject.reparentTo(self.frame)
+        
+        self.hugpos = hugpos
+        self.offset = offset
+        self.redraw(aspect)
+
+    def redraw(self, aspect, flag="wide"):
+        if self.hugpos == "topleft":
+            p = base.a2dTopLeft.getPos()#@UndefinedVariable
+            p.setX(p.getX() + self.offset.getX() + 0.05)
+            p.setZ(p.getZ() + self.offset.getZ() - 0.05)
+        self.frame.setPos(p)
+        if flag == "wide":
+            posx, posy = self.frame.getTightBounds()
+            self.pos_min_x = posx.getX() / aspect
+            self.pos_min_y = posx.getZ()
+            self.pos_max_x = posy.getX() / aspect
+            self.pos_max_y = posy.getZ()
+        elif flag == "tall":
+            posx, posy = self.frame.getTightBounds()
+            self.pos_min_x = posx.getX()
+            self.pos_min_y = posx.getZ() / aspect
+            self.pos_max_x = posy.getX()
+            self.pos_max_y = posy.getZ() / aspect
+            
+    def turnOn(self):
+        self.imageObject.setImage(self.name+"_on.png")#@UndefinedVariable
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        
+    def turnOff(self):
+        self.imageObject.setImage(self.name+".png")#@UndefinedVariable
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)    
+            
+    def enable(self):
+        self.imageObject.setImage(self.name+".png")#@UndefinedVariable
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        self.enabled = True
+        
+    def disable(self):
+        self.imageObject.setImage("empty.png")#@UndefinedVariable
+        self.imageObject.setTransparency(TransparencyAttrib.MAlpha)
+        self.enabled = False
+            
+    def removeNode(self):
+        self.frame.removeNode()   
+                      
+class GuiTextFrame:
+    def __init__(self, offset, h_size, v_size, numLines, parent):
+        self.numLines = numLines
+        self.frame = DirectFrame(   relief = DGG.FLAT
+                                  , frameColor = (0.2, 0.2, 0.2, 0.8)
+                                  , scale = 1
+                                  , frameSize = (0, h_size, 0, -v_size) )
+        
+        self.frame.reparentTo(parent)#@UndefinedVariable
+        self.offset = offset
+        self.frame.setPos(self.offset.getX(), 0, self.offset.getZ())
+
+        fixedWidthFont = loader.loadFont("monommm_5.ttf")#@UndefinedVariable
+        if not fixedWidthFont.isValid():
+            print "pandaInteractiveConsole.py :: could not load the defined font %s" % str(self.font)
+            fixedWidthFont = DGG.getDefaultFont()
+        
+        if numLines == 1:
+            self.lineHeight = 0.05
+        else:
+            self.lineHeight = v_size*0.9 / numLines
+            
+        # output lines
+        self.frameOutputList = list()
+        for i in xrange( self.numLines ):
+            label = OnscreenText( parent = self.frame
+                              , text = ""
+                              , pos = (0.005, -(i+1)*self.lineHeight)
+                              , align=TextNode.ALeft
+                              , mayChange=1
+                              , scale=0.04
+                              , fg = (1,1,1,1)
+                              , shadow = (0, 0, 0, 1))
+                              #, frame = (200,0,0,1) )
+            label.setFont( fixedWidthFont )
+            self.frameOutputList.append( label )
+
+    def write(self, lineNumber, text):
+        if lineNumber > self.numLines:
+            return
+        self.frameOutputList[lineNumber - 1].setText(text)
+        
+    def redraw(self):
+        p = base.a2dTopLeft.getPos()#@UndefinedVariable
+        p.setX(p.getX() + self.offset.getX())
+        p.setZ(p.getZ() - 0.05)
+        self.frame.setPos(p)
+
+class GuiUnitInfo:
+    def __init__(self, offset, parent):
+
+        fixedWidthFont = loader.loadFont("monommm_5.ttf")#@UndefinedVariable
+        if not fixedWidthFont.isValid():
+            print "pandaInteractiveConsole.py :: could not load the defined font %s" % str(self.font)
+            fixedWidthFont = DGG.getDefaultFont()
+        
+        self.label = OnscreenText( parent = parent
+                              , text = ""
+                              , pos = (offset.getX(),offset.getZ())
+                              , align=TextNode.ACenter
+                              , mayChange=True
+                              , scale=0.14
+                              , fg = (1,0,0,1)
+                              #, shadow = (0, 0, 0, 1)
+                              #, frame = (200,0,0,1) 
+                              )
+        self.label.setFont( fixedWidthFont )
+        self.label.setLightOff()
+
+
+    def write(self, text):
+        self.label.setText(text)
+        
+    def redraw(self):
+        return
+
+    def remove(self):
+        self.label.remove()
