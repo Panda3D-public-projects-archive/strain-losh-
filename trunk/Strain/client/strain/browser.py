f468295ab860136c24e06a7d6a8fe24b21681fd8
vznidar@gmail.com <vznidar@gmail.com@4f0e03c3-8530-09c2-737e-33c5a6d01d0a>
2012-04-02 19:48:13 +0000

    -converted the menu to librocket

--- trunk/Strain/client/strain/browser.py
+++ trunk/Strain/client/strain/browser.py
@@ -8,7 +8,7 @@
 # panda3D imports
 from panda3d.core import TextNode#@UnresolvedImport
 from direct.gui.DirectGui import DirectButton, DirectLabel#@UnresolvedImport
-
+from panda3d.rocket import *
 
 # strain related imports
 
@@ -24,28 +24,51 @@ class Browser():
         self.username.reparentTo(aspect2d)
         self.username.setPos(-0.7, 0, 0.9)
         
-        self.but_new = DirectButton(text = ("Start new game"),scale=.05,command=self.newGame,text_font=font, text_align=TextNode.ACenter)
-        self.but_cont = DirectButton(text = ("Continue game"),scale=.05,command=self.contGame,text_font=font, text_align=TextNode.ACenter)
-        self.but_obs = DirectButton(text = ("Observe game"),scale=.05,command=self.obsGame,text_font=font, text_align=TextNode.ACenter)
-        self.but_rep = DirectButton(text = ("View replay"),scale=.05,command=self.viewReplay,text_font=font, text_align=TextNode.ACenter)
-        
-        self.but_new.reparentTo(aspect2d)
-        self.but_new.setPos(-0.8, 0, 0.7)
-        self.but_cont.reparentTo(aspect2d)
-        self.but_cont.setPos(-0.8, 0, 0.6)
-        self.but_obs.reparentTo(aspect2d)
-        self.but_obs.setPos(-0.8, 0, 0.5)
-        self.but_rep.reparentTo(aspect2d)
-        self.but_rep.setPos(-0.8, 0, 0.4)
+#        self.but_new = DirectButton(text = ("Start new game"),scale=.05,command=self.newGame,text_font=font, text_align=TextNode.ACenter)
+#        self.but_cont = DirectButton(text = ("Continue game"),scale=.05,command=self.contGame,text_font=font, text_align=TextNode.ACenter)
+#        self.but_obs = DirectButton(text = ("Observe game"),scale=.05,command=self.obsGame,text_font=font, text_align=TextNode.ACenter)
+#        self.but_rep = DirectButton(text = ("View replay"),scale=.05,command=self.viewReplay,text_font=font, text_align=TextNode.ACenter)
+#        
+#        self.but_new.reparentTo(aspect2d)
+#        self.but_new.setPos(-0.8, 0, 0.7)
+#        self.but_cont.reparentTo(aspect2d)
+#        self.but_cont.setPos(-0.8, 0, 0.6)
+#        self.but_obs.reparentTo(aspect2d)
+#        self.but_obs.setPos(-0.8, 0, 0.5)
+#        self.but_rep.reparentTo(aspect2d)
+#        self.but_rep.setPos(-0.8, 0, 0.4)
     
+
+        self.parent.fsm.rRegion.setActive(1)
+        self.parent.fsm.rContext = self.parent.fsm.rRegion.getContext()
+        
+        #context.LoadDocument('assets/background.rml').Show()
+        
+        doc = self.parent.fsm.rContext.LoadDocument('data/rml/game.rml')
+        doc.Show()
+        
+        
+        
+        element = doc.GetElementById('new')
+        element.AddEventListener('click', self.newGame, True)
+        element = doc.GetElementById('continue')
+        element.AddEventListener('click', self.contGame, True)
+        element = doc.GetElementById('obs')
+        element.AddEventListener('click', self.obsGame, True)
+        element = doc.GetElementById('replay')
+        element.AddEventListener('click', self.viewReplay, True)
+#        element = doc.GetElementById('main_menu')
+#        element.AddEventListener('click', self.backToMain, True)
+        
     def cleanup(self):
         self.label_username.remove()
         self.username.remove()
-        self.but_new.remove()
-        self.but_cont.remove()
-        self.but_obs.remove()
-        self.but_rep.remove()
-        
+#        self.but_new.remove()
+#        self.but_cont.remove()
+#        self.but_obs.remove()
+#        self.but_rep.remove()
+        self.parent.fsm.rRegion.setActive(0)
+        self.parent.fsm.rContext.UnloadAllDocuments()
         self.parent = None
     
     def newGame(self):
@@ -60,3 +83,5 @@ class Browser():
     def viewReplay(self):
         None
         
+    def backToMain(self):
+        self.parent.fsm.request('Login')
