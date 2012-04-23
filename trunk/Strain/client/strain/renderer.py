acd5c554ed3b4214ef15c8f87f0593e9600a02d7
ogi.losh@gmail.com <ogi.losh@gmail.com@4f0e03c3-8530-09c2-737e-33c5a6d01d0a>
2012-04-23 14:47:58 +0000

    -added fog of war display (toggle 'B') -added grid display (toggle 'Z')

--- trunk/Strain/client/strain/renderer.py
+++ trunk/Strain/client/strain/renderer.py
@@ -10,7 +10,11 @@ class LevelRenderer(DirectObject):
         self.maxX = 0
         self.maxY = 0
         self.grid_display = False
+        self.fow_display = False
         self.accept('z', self.toggleGrid)
+        self.accept('b', self.toggleFow)
+        
+        self.fow_coef = 2
     
     def load(self, name, x, y):
         name = 'LevelTexturedBlockout'
@@ -19,70 +23,107 @@ class LevelRenderer(DirectObject):
         m = loader.loadModel(name)
         m.setPos(24, 24, 0)
         m.reparentTo(self.node)
+
+        plane = self.node.find("**/Plane.001")
+        plane.removeNode()
+        
+        plane = self.node.find("**/Plane.003")
+        plane.removeNode()
+        
+        plane = self.node.find("**/Plane.004")
+        plane.removeNode()
+        
+        plane = self.node.find("**/Plane.007")
+        plane.removeNode()
+        
         self.createGrid()
-        self.fowImage = PNMImage(x, y)
-        self.fowImage.fill(.1)
-        self.fowBrush = PNMBrush.makeSpot(VBase4D(1), 1, False)
+        
+        self.fowImage = PNMImage(x*self.fow_coef, y*self.fow_coef)
+        self.fowImage.fill(.4)
+
+        #self.fowBrush = PNMBrush.makePixel((1, 1, 1, 1))
+        self.fowBrush = PNMBrush.makeSpot(VBase4D(1), int(self.fow_coef/2), False)
         self.fowPainter = PNMPainter(self.fowImage)
         self.fowPainter.setPen(self.fowBrush)
         self.fowTexture = Texture()
         self.fowTexture.load(self.fowImage)
-        
+            
         minb, maxb = self.node.getTightBounds()
         dim = maxb-minb
         maxDim = max(dim[0],dim[1])
         scale = 1./maxDim
         center = (minb+maxb)*.5
-            
         self.fowTextureStage = TextureStage('')
-        #self.node.setTexture(self.fowTextureStage, self.fowTexture)
-        self.node.setTexGen(self.fowTextureStage, TexGenAttrib.MWorldPosition)
+        self.node.setTexGen(self.fowTextureStage, RenderAttrib.MWorldPosition)
         self.node.setTexScale(self.fowTextureStage, scale)
-        self.node.setTexOffset(self.fowTextureStage, -.5-center[0]*scale, -.5-center[1]*scale)
-
-        # a dummy node to hold size & position of terrain to texture transform 
-        self.texOrigin = render.attachNewNode('')
-        self.texOrigin.setPos(center[0]-.5*maxDim, center[1]+.5*maxDim, 0)
-        self.texOrigin.setScale(maxDim/32,-maxDim/32,1)
+        self.node.setTexOffset(self.fowTextureStage, -.5-center[0]*scale, -.5-center[1]*scale)  
         
         cm = CardMaker('')
-        cm.setFrame(-.4,0,0,.4)
+        cm.setFrame(-.8,-.2,0,.6)
         card = base.a2dBottomRight.attachNewNode(cm.generate())
         card.setTexture(self.fowTexture)
         
     def getInvisibleTiles(self):
+        self.fowImage.fill(.4)
+        
+        k = int(self.fow_coef/2) - 0.5
+        """
+        self.fowPainter.drawPoint((0)*self.fow_coef+k, (0)*self.fow_coef+k)
+        self.fowPainter.drawPoint((5)*self.fow_coef+k, (0)*self.fow_coef+k)  
+        self.fowPainter.drawPoint((10)*self.fow_coef+k, (0)*self.fow_coef+k)  
+        self.fowPainter.drawPoint((15)*self.fow_coef+k, (0)*self.fow_coef+k)
+        
+        self.fowPainter.drawPoint((20)*self.fow_coef+k, (23)*self.fow_coef+k)    
+        self.fowTexture.load(self.fowImage)       
+        return
+        
+        """
+        
+        """
+        self.fowPainter.drawRectangle(32, 32, 47, 47) 
+        self.fowTexture.load(self.fowImage)                                                              
+        return
+        """
+        
+        
         tile_dict = self.parent.parent.getInvisibleTiles()
-        n = NodePath('')
-        for invisible_tile in tile_dict:      
-            n.setPos(utils.TILE_SIZE*(invisible_tile[0]+0.5), utils.TILE_SIZE*(invisible_tile[1]+0.5), utils.GROUND_LEVEL)
-            x = n.getX(self.texOrigin)
-            y = n.getY(self.texOrigin)
-            print invisible_tile[0], invisible_tile[1], x,y
-            self.fowPainter.drawPoint(x, y)
-            self.fowTexture.load(self.fowImage)
+        for invisible_tile in tile_dict:
+            if tile_dict[invisible_tile] != 0:
+                self.fowPainter.drawPoint(invisible_tile[0]*self.fow_coef+k, (23-invisible_tile[1])*self.fow_coef+k)
+                #self.fowPainter.drawRectangle(invisible_tile[0]*self.fow_coef, (23-invisible_tile[1])*self.fow_coef, (invisible_tile[0]+1)*self.fow_coef-1, (24-invisible_tile[1])*self.fow_coef-1)                                                               
+        self.fowTexture.load(self.fowImage)
     
-    def createGrid(self, thickness=3.0, color=Vec4(1,1,0,0.5)):
+    def createGrid(self, thickness=1.0, color=Vec4(0.96,0.64,0.37,1)):
         segs = LineSegs()
         segs.setThickness(thickness)
         segs.setColor(color)
         for i in xrange(self.maxX):
-            segs.moveTo(i*utils.TILE_SIZE, 0, utils.GROUND_LEVEL)
-            segs.drawTo(i*utils.TILE_SIZE, self.maxY*utils.TILE_SIZE, utils.GROUND_LEVEL+0.02)
+            segs.moveTo(i*utils.TILE_SIZE, 0, utils.GROUND_LEVEL+0.05)
+            segs.drawTo(i*utils.TILE_SIZE, self.maxY*utils.TILE_SIZE, utils.GROUND_LEVEL+0.05)
         for j in xrange(self.maxY):
-            segs.moveTo(0, j*utils.TILE_SIZE, utils.GROUND_LEVEL)
-            segs.drawTo(self.maxX*utils.TILE_SIZE, j*utils.TILE_SIZE, utils.GROUND_LEVEL+0.02)
+            segs.moveTo(0, j*utils.TILE_SIZE, utils.GROUND_LEVEL+0.05)
+            segs.drawTo(self.maxX*utils.TILE_SIZE, j*utils.TILE_SIZE, utils.GROUND_LEVEL+0.05)
         self.grid = NodePath(segs.create())
-        self.grid.setTransparency(TransparencyAttrib.MAlpha)
+        #self.grid.setTransparency(TransparencyAttrib.MAlpha)
         self.grid.setLightOff()
-        self.grid.setDepthOffset(2)
-    
+        self.grid.setDepthOffset(3)
+     
     def toggleGrid(self):
         if not self.grid_display:
-            self.grid.reparentTo(self.node)
+            self.grid.reparentTo(render)
             self.grid_display = True
         else:
             self.grid.detachNode()
             self.grid_display = False
+            
+    def toggleFow(self):
+        if not self.fow_display:
+            self.node.setTexture(self.fowTextureStage, self.fowTexture)
+            self.fow_display = True
+    
+        else:
+            self.node.clearTexture(self.fowTextureStage)
+            self.fow_display = False
     
     def createCoordinateDisplay(self):
         for i in xrange(0, self.maxX):
