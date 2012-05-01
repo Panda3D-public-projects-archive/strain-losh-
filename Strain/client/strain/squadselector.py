b4f042d016b48b8d45ca709e95f5c931ef745b3e
vznidar@gmail.com <vznidar@gmail.com>
2012-05-01 09:28:55 +0000

    -codebehind of the new squad selection menu

--- Strain/client/strain/squadselector.py
+++ Strain/client/strain/squadselector.py
@@ -9,7 +9,6 @@ import string
 from panda3d.core import *
 from direct.gui.DirectGui import *
 from panda3d.rocket import *
-from squaddatasource import *
 # strain related imports
 
 
@@ -17,7 +16,6 @@ from squaddatasource import *
 #
 
 unit_dict = {}
-unit_dict['NONE'] = {'name':'NONE', 'cost':0}
 unit_dict['Standard'] = {'name':'Standard', 'cost':100}
 unit_dict['Sergeant'] = {'name':'Sergeant', 'cost':150}
 unit_dict['Medic'] = {'name':'Medic', 'cost':100}
@@ -28,129 +26,127 @@ unit_dict['Jumper'] = {'name':'Jumper', 'cost':100}
 
 name_list = ['NONE (0)', 'Standard (100)', 'Sergeant (150)', 'Medic (100)', 'Heavy (100)', 'Scout (100)', 'Jumper (100)']
 
-class Selector():
-    def __init__(self, parent, x, y, i):
-        self.parent = parent
-        self.cost = 0
-        font = loader.loadFont('monommm_5.ttf')
-        self.dom = DirectOptionMenu(text="Unit", scale=0.05, items=name_list, initialitem=0, highlightColor=(1,0,0,1),command=self.itemSel
-                            ,relief=DGG.RAISED, popupMarkerBorder = (.5, .5), pressEffect=0, text_scale=0.8, item_text_font=font, text_font=font)
-
-        self.dom.setPos(x, 0, y)
-        self.dom.reparentTo(self.parent.root)
-        
-    def itemSel(self, arg):
-        ind = arg.find(' ')
-        arg_mod = arg[0:ind]
-        cost_diff = self.cost - unit_dict[arg_mod]['cost'] 
-        
-        if self.parent.budget + cost_diff < 0:
-            self.dom['text'] = 'NONE (0)'
-        else:
-            self.cost = unit_dict[arg_mod]['cost']
-            self.parent.refreshBudget(cost_diff)
-       
 class SquadSelector():
     def __init__(self, parent, player, budget):
         self.parent = parent
         self.player = player
         self.player_id = None
 
-        #self.root = aspect2d.attachNewNode('root_selector')
-
         self.budget = budget
         self.current_sum = 0
         
         self.pickers = []
         
-#        for i in xrange(10):
-#            s = Selector(self, -1, 0.8-i/10., i)
-#            self.pickers.append(s)
-#        
-#            
-#        p = DirectLabel(text=self.player, scale=0.05)
-#        p.setPos(-1, 0, 0.9)
-#        p.reparentTo(self.root)
-#        
-#        self.b = DirectLabel(text=str(self.budget), scale=0.05)
-#        self.b.setPos(-0.7, 0, 0.9)
-#        self.b.reparentTo(self.root)
-#        
-#        self.go = DirectButton(text='GO!', command=self.go)
-#        self.go.setPos(0,0,0)
-#        self.go.setScale(0.05)
-#        self.go.reparentTo(self.root)
-#        
-#        self.cancel = DirectButton(text='Cancel!', command=self.cleanup)
-#        self.cancel.setPos(0,0,-0.2)
-#        self.cancel.setScale(0.05)
-#        self.cancel.reparentTo(self.root)
-    
         self.parent.rRegion.setActive(1)
         self.parent.rContext = self.parent.rRegion.getContext()
         
-        self.squadData = SquadDataSource("squad_data", unit_dict)
-        self.squadDataFormatter = SquadDataFormatter("frm")
-        
         self.doc = self.parent.rContext.LoadDocument('data/rml/squad.rml')
+        
+        inner_rml = " "
+        for unit in unit_dict:
+            name = unit_dict[unit]['name']
+            cost = unit_dict[unit]['cost']
+            inner_rml += '<div id="' + name + '" class="list_item"><div class="list_item_filler"></div><div class="item_image">'
+            inner_rml += '<img src="../textures/unit_' + name.lower() + '_big_transparent_32.png" width="32px" height="32px" alt="" title="" />' 
+            inner_rml += '</div><div class="item_text"><div class="item_name">' + name + '</div><div class="item_cost">Cost:' + str(cost) + '</div></div></div>'
+        
+        element = self.doc.GetElementById('list_container')
+        element.inner_rml = inner_rml
+        
+        for child in element.child_nodes:
+            child.AddEventListener('click', self.selectUnit, True)
+        
+        element = self.doc.GetElementById('btn_recruit')
+        element.AddEventListener('click', self.recruitUnit, True)
+
+        element = self.doc.GetElementById('btn_deploy')
+        element.AddEventListener('click', self.deploySquad, True)
+        
+        funds_label = self.doc.GetElementById('funds')
+        funds_label.inner_rml = 'Funds left: ' + str(self.budget)
+            
         self.doc.Show()
         
-        for i in xrange(1, 9):
-            ddId = "soldier" + str(i)
-            element = self.doc.GetElementById(ddId)
-            element.value = 'NONE'
-            element.AddEventListener('change', self.itemChanged, True)
-            self.pickers.append(element)
-                   
-        element = self.doc.GetElementById('ok')
-        element.AddEventListener('click', self.go, True)
-        element = self.doc.GetElementById('cancel')
-        element.AddEventListener('click', self.cleanup, True)
-
-        element = self.doc.GetElementById('plr')
-        element.inner_rml = self.player
-        element = self.doc.GetElementById('bgt')
-        element.inner_rml = str(self.budget)
-
-    def go(self):
+    def deploySquad(self):
         self.parent.deploy_queue = []
         self.parent.deployed_dict = {}
         for p in self.pickers:
-            l = p.value
-            #ind = l.find(' ')
-            #l_mod = l[0:ind]
-            if l != 'NONE':
-                self.parent.deploy_queue.append(l)
+            self.parent.deploy_queue.append(p)
         self.parent.fsm.request('Deploy')
+
+    def removeUnit(self):
+        i = 0
+        for child in event.current_element.parent_node.child_nodes:
+            if child == event.current_element:
+                break
+            i = i + 1
+
+        self.pickers.pop(i)
+        inner_rml = self.pickersToRML()
+        event.current_element.parent_node.inner_rml = inner_rml
+        self.refreshFunds()
+        self.addSquadItemEvents()
+
+    def recruitUnit(self):
+        
+        if len(self.pickers) == 12:
+            return
+        
+        unit_id = None
+        list = self.doc.GetElementById('list_container')
+        for list_item in list.child_nodes:
+            if list_item.class_name == 'list_item_selected':
+                unit_id = list_item.id
+                break
+        
+        if unit_id == None:
+            return
+
+        if (self.current_sum + unit_dict[unit_id.capitalize()]['cost']) > self.budget:
+            return
+        
+        self.pickers.append(unit_id)
+        inner_rml = self.pickersToRML()
+        squad = self.doc.GetElementById('squad_container')
+        squad.inner_rml = inner_rml
+        self.refreshFunds()
+        self.addSquadItemEvents()
     
+    def pickersToRML(self):
+        inner_rml = ''
+        for p in self.pickers:
+            inner_rml += '<div class="unit_item"><div class="unit_item_image">'
+            inner_rml += '<img src="../textures/unit_' + p.lower() + '_big_transparent_32.png" width="32px" height="32px" alt="" title="" />'
+            inner_rml += '</div></div>'
+        
+        for i in xrange(1, 13 - len(self.pickers)):
+            inner_rml += '<div class="unit_item_empty"></div>'
+            
+        return inner_rml
+    
+    def addSquadItemEvents(self):
+        element = self.doc.GetElementById('squad_container')
+        for child in element.child_nodes:
+            if child.class_name == 'unit_item': 
+                child.AddEventListener('click', self.removeUnit, True)
+    
+    def selectUnit(self):
+        for child in event.current_element.parent_node.child_nodes:
+            child.class_name = "list_item"
+        event.current_element.class_name = "list_item_selected"
+
+    def refreshFunds(self):
+        funds = 0
+        for p in self.pickers:
+            funds += unit_dict[p.capitalize()]['cost']
+            
+        self.current_sum = funds
+        
+        funds_label = self.doc.GetElementById('funds')
+        funds_label.inner_rml = 'Funds left: ' + str(self.budget - self.current_sum)
+        
     def cleanup(self):
-        #self.root.removeNode()
         self.parent.rRegion.setActive(0)
         self.parent.rContext.UnloadAllDocuments()
-    
-    def refreshBudget(self, val):
-        self.budget += val
-        self.b['text'] = str(self.budget)
         
-    def itemChanged(self):
-        self.current_sum = 0
-        for element in self.pickers:
-            value = unit_dict[element.value]['cost']
-            if self.current_sum + int(value) > self.budget:
-                element.value = 'NONE'
-            else:
-                self.current_sum += int(value)
         
-        element = self.doc.GetElementById('bgt')
-        element.inner_rml = str(self.budget - self.current_sum)
-
-
-
-        
-
-
-
-
-        
-    
-            
