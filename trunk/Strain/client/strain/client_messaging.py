71069b6929ca6c320b46c3d53639ed89f4a84297
krav.losh@gmail.com <krav.losh@gmail.com@4f0e03c3-8530-09c2-737e-33c5a6d01d0a>
2012-08-02 20:17:08 +0000

    -afas

--- trunk/Strain/client/strain/client_messaging.py
+++ trunk/Strain/client/strain/client_messaging.py
@@ -294,7 +294,7 @@ class ClientMsg:
     @staticmethod
     def enterGame( game_id ):
         ClientMsg.game_id = game_id
-        ClientMsg._sendMsg( (STERNER_ID, ENTER_GAME) )
+        ClientMsg._sendMsg( (STERNER_ID, ENTER_GAME, game_id) )
         
         
     @staticmethod
